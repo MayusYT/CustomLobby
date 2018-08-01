c3119786c3dcb8644c66289c26142302447e7d5f
MayusYT <mariusschuh@outlook.de>
2018-08-01 18:20:45 +0200

    Initial Commit Added Structure

---
+++ src/customlobby/banmanager/Banmanager.java
@@ -0,0 +1,4 @@
+package customlobby.banmanager;
+
+public class Banmanager {
+}
--- src/customlobby/essential/LobbyRestrictions.java
+++ src/customlobby/essential/LobbyRestrictions.java
@@ -1,6 +1,53 @@
 package customlobby.essential;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Weather;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.weather.ThunderChangeEvent;
+import org.bukkit.event.weather.WeatherChangeEvent;
 
 public class LobbyRestrictions implements Listener {
+
+    @EventHandler
+    public void onHunger(FoodLevelChangeEvent e) {
+        e.setCancelled(true);
+        e.setFoodLevel(20);
+    }
+    @EventHandler
+    public void onDamage(EntityDamageEvent e) {
+        e.setCancelled(true);
+    }
+
+    @EventHandler(priority=EventPriority.HIGHEST)
+    public void onWeatherChange(WeatherChangeEvent event) {
+
+        boolean rain = event.toWeatherState();
+        if(rain)
+            event.setCancelled(true);
+    }
+
+    @EventHandler(priority=EventPriority.HIGHEST)
+    public void onThunderChange(ThunderChangeEvent event) {
+
+        boolean storm = event.toThunderState();
+        if(storm)
+            event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onPickup(PlayerPickupItemEvent e) {
+        if(!e.getPlayer().hasPermission("CustomLobby.pickupItems")) {
+            e.setCancelled(true);
+        }
+    }
+
+
 }
