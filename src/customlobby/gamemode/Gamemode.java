6b00cc45ad2c9b25b0ea977ac00104f43973b1f8
Tallerik <erik.klasse@gmail.com>
2018-08-02 15:00:55 +0200

    Update /c

--- src/customlobby/gamemode/Gamemode.java
+++ src/customlobby/gamemode/Gamemode.java
@@ -1,12 +1,23 @@
 package customlobby.gamemode;
 
+import customlobby.utils.API;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Gamemode implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        return false;
+        if(cmd.getName().equalsIgnoreCase("c")) {
+            if(sender.hasPermission("CustomLobby.Gamemode.c")) {
+                ((Player)sender).setGameMode(GameMode.CREATIVE);
+                sender.sendMessage(API.getPrefix() + "§a Du bist nun im §6Kreativmodus§a!");
+            } else {
+                sender.sendMessage(API.getNoPermString());
+            }
+        }
+        return true;
     }
 }
