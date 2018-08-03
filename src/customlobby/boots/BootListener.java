a064fa98320eb1f7c97df57fc3ce288379676737
Tallerik <erik.klasse@gmail.com>
2018-08-03 15:22:40 +0200

    Update Boots

--- src/customlobby/boots/BootListener.java
+++ src/customlobby/boots/BootListener.java
@@ -16,24 +16,35 @@ public class BootListener implements Listener {
     public void onMove(PlayerMoveEvent e) {
         Player p = e.getPlayer();
         if(p.getInventory().getBoots() != null) {
+            //Diamond Boots
             if(p.getInventory().getBoots().equals(new ItemStack(Material.DIAMOND_BOOTS))) {
                 p.setAllowFlight(true);
             } else {
-
                     p.setAllowFlight(false);
-
-
             }
-        } else {
-            if(p.getGameMode() == GameMode.SURVIVAL) {
-                p.setAllowFlight(false);
+
+            //Gold Boots
+            if(p.getInventory().getBoots().equals(new ItemStack(Material.GOLD_BOOTS))) {
+                p.setWalkSpeed(0.6f);
             } else {
-                p.setAllowFlight(true);
+                p.setWalkSpeed(0.2f);
             }
 
-        }
+            //Chainmail Boots
+            if(p.getInventory().getBoots().equals(new ItemStack(Material.CHAINMAIL_BOOTS))) {
+                p.setPlayerListName("§k" + p.getName() + "§r");
+            } else {
+                p.setPlayerListName(p.getName());
+            }
 
+        } else {
+            p.setAllowFlight(false);
+            p.setWalkSpeed(0.2f);
+            p.setPlayerListName(p.getName());
+        }
     }
+
+
 
 
 }
